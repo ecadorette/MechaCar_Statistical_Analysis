f7fc5fb9ea3358a04c0d1a836f2e9abf3596f25e
Elizabeth Cadorette <liz.cadorette@gmail.com>
2023-03-12 17:40:02 -0400

    Final Upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,5 +15,9 @@ Suspension_coil <-read.csv(file='Suspension_Coil.csv', check.names = F,stringsAs
 total_summary <- Suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
 #create a lot_summary dataFrame  of PSI
 lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
-
-
+# t-test for all manufacturing lots against population
+t.test(Suspension_coil$PSI,mu=1500)
+# t-test for each manufacturing list
+t.test(subset(Suspension_coil,Manufacturing_Lot=='Lot1')$PSI,mu=1500)
+t.test(subset(Suspension_coil,Manufacturing_Lot=='Lot2')$PSI,mu=1500)
+t.test(subset(Suspension_coil,Manufacturing_Lot=='Lot3')$PSI,mu=1500)
