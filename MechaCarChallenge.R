6e05f954d1a9be89232a2a94be8c91fc4f9c129a
Elizabeth Cadorette <liz.cadorette@gmail.com>
2023-03-12 16:43:13 -0400

    progress upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,4 +8,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car))
 
+#DELIVERABLE 2
+#read in Suspension_Coil.csv
+Suspension_coil <-read.csv(file='Suspension_Coil.csv', check.names = F,stringsAsFactors = F)
+#create a summary dataFrame of PSI
+total_summary <- Suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+#create a lot_summary dataFrame  of PSI
+lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
 
